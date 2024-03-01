ea06699b3df42aa6515f832e9d358e56a5715995
mehmeteminburakoksuz <meboksuz02@gmail.com>
2024-03-01 13:40:21 +0300

    Sign Up , Login and Home page

--- app/src/main/java/com/fitapp/SignupActivity.java
+++ app/src/main/java/com/fitapp/SignupActivity.java
@@ -2,70 +2,109 @@ package com.fitapp;
 
 
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import com.fitapp.databinding.ActivitySignupBinding;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
+import java.util.Objects;
 
 
 public class SignupActivity extends AppCompatActivity {
 
-    ActivitySignupBinding binding;
-    DatabaseHelper databaseHelper;
 
+    private EditText editEmail, editPassword,editName;
+    private String txtEmail, txtPassword, txtName;
+
+    private FirebaseUser mUser;
+
+    private FirebaseAuth mAuth;
+
+    private DatabaseReference mReference;
+
+    private HashMap<String, Object> mData;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        binding = ActivitySignupBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
+        setContentView(R.layout.activity_signup);
+        editEmail= (EditText) findViewById(R.id.kayit_ol_mail);
+        editPassword =(EditText) findViewById(R.id.kayit_ol_password);
+        editName = (EditText) findViewById(R.id.kayit_ol_nickname);
 
-        databaseHelper = new DatabaseHelper(this);
 
-        binding.signupButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                String email = binding.signupEmail.getText().toString();
-                String password = binding.signupPassword.getText().toString();
-                String confirmPassword = binding.signupConfirm.getText().toString();
-
-                if(email.equals("")||password.equals("")||confirmPassword.equals(""))
-                    Toast.makeText(SignupActivity.this, "All fields are mandatory", Toast.LENGTH_SHORT).show();
-                else{
-                    if(password.equals(confirmPassword)){
-                        Boolean checkUserEmail = databaseHelper.checkEmail(email);
-
-                        if(checkUserEmail == false){
-                            Boolean insert = databaseHelper.insertData(email, password);
-
-                            if(insert == true){
-                                Toast.makeText(SignupActivity.this, "Signup Successfully!", Toast.LENGTH_SHORT).show();
-                                Intent intent = new Intent(getApplicationContext(),LoginActivity.class);
-                                startActivity(intent);
-                            }else{
-                                Toast.makeText(SignupActivity.this, "Signup Failed!", Toast.LENGTH_SHORT).show();
-                            }
-                        }
-                        else{
-                            Toast.makeText(SignupActivity.this, "User already exists! Please login", Toast.LENGTH_SHORT).show();
-                        }
-                    }else{
-                        Toast.makeText(SignupActivity.this, "Invalid Password!", Toast.LENGTH_SHORT).show();
-                    }
-                }
-            }
-        });
-
-        binding.loginRedirectText.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(SignupActivity.this, LoginActivity.class);
-                startActivity(intent);
-            }
-        });
+        mAuth = FirebaseAuth.getInstance();
+        mReference = FirebaseDatabase.getInstance().getReference();
 
     }
+
+    public void kayitOl(View view){
+
+        txtName = editName.getText().toString();
+        txtEmail = editEmail.getText().toString();
+        txtPassword = editPassword.getText().toString();
+
+        if(!TextUtils.isEmpty(txtName) && !TextUtils.isEmpty(txtEmail) && !TextUtils.isEmpty(txtPassword)){
+            mAuth.createUserWithEmailAndPassword(txtEmail,txtPassword)
+                    .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()){
+                                mUser = mAuth.getCurrentUser();
+
+
+                                mData = new HashMap<>();
+                                mData.put("Username", txtName);
+                                mData.put("User Mail : ", txtEmail);
+                                mData.put("UserPassword", txtPassword);
+                                mData.put("User Id", mUser.getUid());
+
+
+                                mReference.child("Users").child(mUser.getUid())
+                                        .setValue(mData)
+                                        .addOnCompleteListener(SignupActivity.this, new OnCompleteListener<Void>() {
+                                            @Override
+                                            public void onComplete(@NonNull Task<Void> task) {
+
+                                                if (task.isSuccessful()){
+                                                    Toast.makeText(SignupActivity.this,"Registratin Successful",Toast.LENGTH_SHORT).show();
+                                                    Intent intent = new Intent(SignupActivity.this, LoginActivity.class);
+                                                    startActivity(intent);
+
+                                                }else
+                                                    Toast.makeText(SignupActivity.this,task.getException().getMessage(),Toast.LENGTH_SHORT).show();
+
+                                            }
+                                        });
+
+                            }
+
+                            else
+                                Toast.makeText(SignupActivity.this,task.getException().getMessage(),Toast.LENGTH_SHORT).show();
+
+                        }
+                    });
+        }else
+            Toast.makeText(this, "Email Ve Şifre Boş Olamaz",Toast.LENGTH_SHORT ).show();
+
+    }
+
+
+
+
+
 }
